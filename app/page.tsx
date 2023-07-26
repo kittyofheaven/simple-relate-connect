3ea3aa5df0480f41086fe6a93d9a6caf383009c7
Hazel Handrata <hazelhandrata@gmail.com>
2023-07-27 02:04:57 +0700

    fix[ / ]: change img to Image resolve error in vercell

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 
 let categories:string = 'romanticizer'
@@ -16,11 +17,11 @@ export default function Home() {
             <div className='bg-white rounded-xl shadow-lg'>
               <div className='p-5 flex flex-col'>
                 <div className='rounded-xl overflow-hidden border-black border-b-4 border-r-4'>
-                  <img src='https://commit30.com/cdn/shop/products/WeeklyDayPlannerInside_1280x.jpg?v=1688748909' alt='' />
+                  <Image src='/autoDatePlanner.jpeg' alt='' width={400} height={400} />
                 </div>
                 <h5 className='text-2xl font-medium mt-3 '>Auto Date Planner</h5>
                 <p className='text-md mt-3'>Ask an AI to plan a date for you, since you're too busy thinking about everything else! You may be busy but a date is always necessary if you still want to have a relationship. </p>
-                <a href='/dateplanner' className='border-b-4 border-r-4 text-center bg-pink-300 text-pink-700 py-2 rounded-lg font-semibold mt-6 hover:bg-pink-500 focus:scale-95 transition-all duration-200 ease-out'>Try it!</a>
+                <Link href='/dateplanner' className='border-b-4 border-r-4 text-center bg-pink-300 text-pink-700 py-2 rounded-lg font-semibold mt-6 hover:bg-pink-500 focus:scale-95 transition-all duration-200 ease-out'>Try it!</Link>
               </div>
             </div>
 
@@ -28,11 +29,11 @@ export default function Home() {
             <div className='bg-white rounded-xl shadow-lg'>
               <div className='p-5 flex flex-col'>
                 <div className='rounded-xl overflow-hidden border-black border-b-4 border-r-4'>
-                  <img src='https://hips.hearstapps.com/hmg-prod/images/img-20200415-110729-1586974804.jpg?crop=0.752xw:1.00xh;0.119xw,0&resize=1200:*' alt='' />
+                  <Image src='/tendencyQuiz.jpg' alt='' width={400} height={400} />
                 </div>
                 <h5 className='text-2xl font-medium mt-3 '>Tendency Quiz</h5>
                 <p className='text-md mt-3'> Sometimes it's hard being honest, but this time it doesn't have to. The journal allows you to write about how your relationship has been going & you could read it again and reflect on it. </p>
-                <a href='/journal' className='border-b-4 border-r-4 text-center bg-pink-300 text-pink-700 py-2 rounded-lg font-semibold mt-6 hover:bg-pink-500 focus:scale-95 transition-all duration-200 ease-out'>Take the Tendency Quiz</a>
+                <Link href='/journal' className='border-b-4 border-r-4 text-center bg-pink-300 text-pink-700 py-2 rounded-lg font-semibold mt-6 hover:bg-pink-500 focus:scale-95 transition-all duration-200 ease-out'>Take the Tendency Quiz</Link>
               </div>
             </div>
           
