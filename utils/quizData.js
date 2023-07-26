f860a1c1164d16de64e976570ebf65e83679a57f
Hazel Handrata <hazelhandrata@gmail.com>
2023-07-27 01:18:26 +0700

    feat[quiz, dashboardTips]: add quiz object and tips object

---
+++ utils/quizData.js
@@ -0,0 +1,96 @@
+export const quiz = {
+    totalQuestions: 18,
+    questions: [
+        {
+            id: 1,
+            question: "I don't want to go on a second date with someone if I don't feel the spark when we meet",
+            answers: ['Very unlike me', 'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 2,
+            question: "When I'm on a date I might ask myself, Is this person up to my standards?",
+            answers: ['Very unlike me', 'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 3,
+            question: "I'll be ready to date when I improve myself (for example, lose weight or feel more financially stable)",
+            answers: ['Very unlike me', 'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 4,
+            question: "I'd prefer if my partner and I had a romantic 'how we met' story.",
+            answers: ['Very unlike me', 'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 5,
+            question: "I usually read reviews before I make a significant purchase.",
+            answers: ['Very unlike me', 'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 6,
+            question: "I don't have time to date right now",
+            answers: ['Very unlike me', 'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 7,
+            question: "I believe there's someone out there who's perfect for me, I just haven't met them yet",
+            answers: ['Very unlike me', 'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 8,
+            question: "When making a decision I tend to go back and forth weighing all the possible options.",
+            answers: ['Very unlike me', 'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 9,
+            question: "My friends tell me I need to put myself out there more.",
+            answers: ['Very unlike me', 'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 10,
+            question: "I find the apps unromantic because I want to meet my person in a more natural way.",
+            answers: ['Very unlike me', 'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 11,
+            question: "I pride myself on never settling.",
+            answers: ['Very unlike me', 'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 12,
+            question: "I rarely go on dates.",
+            answers: ['Very unlike me', 'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 13,
+            question: "I don't believe the spark can grow over time. Either you feel it in the beginning, or you don't.",
+            answers: ['Very unlike me', 'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 14,
+            question: "I'll know I've met the right person because I'll feel completely sure about them.",
+            answers: ['Very unlike me', 'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 15,
+            question: "If I want to attract the best possible person, first I need to become the best possible person.",
+            answers: ['Very unlike me', 'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 16,
+            question: "Love is a gut feeling. You know it when you feel it.",
+            answers: ['Very unlike me', 'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 17,
+            question: "My friends think I'm too picky.",
+            answers: ['Very unlike me', 'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 18,
+            question: "I'm focusing on my career now and I'll think about dating",
+            answers: ['Very unlike me', 'Somewhat like me', 'Very like me']
+        },
+    ]
+
+}
