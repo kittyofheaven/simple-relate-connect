19a6a758b206421f0aceca6ee20b8a0ce9137009
Hazel Handrata <hazelhandrata@gmail.com>
2023-07-28 17:51:19 +0700

    refactor[DatePlanner]: kasih loading

--- app/dateplanner/page.tsx
+++ app/dateplanner/page.tsx
@@ -5,6 +5,7 @@ import React, { useState, useEffect } from 'react'
 export default function DatePlanner() {
 
     const [AiResult, setAiResult] = useState('')
+    const [loading, setLoading] = useState(false)
 
     async function getAiResult(place: string, time: string) {
         const cohere = require('cohere-ai');
@@ -26,13 +27,14 @@ export default function DatePlanner() {
     // getAiResult()
 
     useEffect(() => {
-        // console.log(AiResult)
+        
     }, [AiResult])
 
 
     function handleForm(e: any) {
         e.preventDefault()
         getAiResult(e.target[0].value, e.target[1].value)
+        setLoading(true)
     }
 
 
@@ -46,10 +48,17 @@ export default function DatePlanner() {
                     <button className='px-8 py-2 bg-pink-500 flex items-center p4 border-b-4 border-r-4 border-black rounded-md shadow-lg hover:bg-pink-200 ' type="submit">Create!</button>
                 </div>
             </form>
-            {AiResult === '' ? (<></>) : (
-                <div className='bg-white rounded-lg py-4 border-b-4 border-r-4 p-8 text-md text-center mt-8 mx-80'>
-                    {AiResult}
+
+            {loading === false ? (<></>) : (
+
+                <div>
+                    {AiResult === '' ? (<div className='text-2xl text-center mt-10'  >Loading ...</div>) : (
+                        <div className='bg-white rounded-lg py-4 border-b-4 border-r-4 p-8 text-md text-center mt-8 mx-80'>
+                            {AiResult}
+                        </div>
+                    )} 
                 </div>
+
             )}
         </div>
     )
